ddec18d2783a2414e6f386b49a882b86643a6770
vincent kok <vincentkok@live.nl>
2023-08-13 15:33:04 +0200

    Add Azure function project

---
+++ src/TooGoodToGoNotifier.Presentation.FunctionApp/Functions/FavoritesScannerFunction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace TooGoodToGoNotifier.Presentation.FunctionApp.Functions; 
+
+public static class FavoritesScannerFunction {
+    [FunctionName("FavoritesScanner")]
+    public static async Task RunAsync([TimerTrigger("*/1 * * * *")] TimerInfo myTimer, ILogger log) {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+    }
+}
