0c1baf8e5cc46c2116e5af025f1e518cd9490c3d
Vincent Kok <vincentkok@live.nl>
2024-08-06 19:55:55 +0200

    Create resource group using terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,4 @@
+﻿resource "azurerm_resource_group" "tgtg_rg" {
+  location = var.location
+  name     = "rg-tgtgnotifier-app-${var.environment}"
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+﻿variable "environment" {
+  type        = string
+  description = "Environment name"
+  validation {
+    condition     = contains(["dev", "acc", "prd"], var.environment)
+    error_message = "Environment name must be one of: [dev, acc, prd]"
+  }
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+  description = "The location of the resource group"
+}
